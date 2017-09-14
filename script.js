2ef3b93042eb732d5a4d778a0b5e5a8d8ecd93d0
BuildTools <unconfigured@null.spigotmc.org>
2017-09-13 22:20:47 -0400

    Added message for testing purposes

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
     var audio = document.getElementById('audio');
     audio.loop = true;
 });
+
+//SWITCH
+var cmd = document.getElementById('inputText');
+var submitCmd = document.getElementById('inputSubmit');
+
+function submitCmd() {
+    document.getElementById('TEMP') = cmd;
+
+}
