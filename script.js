3aed4e57a973386324d3bc141acc7bdd8f7cf9b6
HairyDragonBeck <hairybeck2@gmail.com>
2017-10-12 16:14:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //SWITCH
 //var cmd = document.getElementById('inputText').value;
-var cmd = document.forms['commandForm']['formInput'].value
+var cmd = document.forms['commandForm']['formInput'].value;
 var mode = 'normal';
 function submitCmd() {
     document.getElementById('commandText') = cmd;
