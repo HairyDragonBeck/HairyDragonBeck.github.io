2b9a866939a4d45cc8684d8f9a398fd55ca977f6
HairyDragonBeck <hairybeck2@gmail.com>
2017-10-16 13:49:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,12 @@
 var mode = 'normal';
 function submitCmd() {
     var x = document.getElementById('inputForm');
-    var text = '';
-    //int i = x.elements.length;
-    //text += x.elements.value;
     //document.getElementById('commandText').innerHTML = text;
     var cmd = document.forms['commandForm']['formInput'].value;
-   document.getElementById('commandText').innerHTML = cmd;
-    
+    if(cmd == 'time') {
+        var date = new Date();
+        var dateMin = date.getMinutes();
+    }
 }
 function submitButton() {
  document.getElementById('inputForm').submit();   
