c70e70b5389f7a9d45078ff771c79f0ce34626f3
HairyDragonBeck <hairybeck2@gmail.com>
2017-10-16 13:41:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function submitCmd() {
     var x = document.getElementById('inputForm');
     var text = '';
     int i = x.elements.length;
-    text += x.elements[i].value;
+    text += x.elements.value;
     document.getElementById('commandText').innerHTML = text;
     //var cmd = document.forms['commandForm']['formInput'].value;
    // document.getElementById('commandText').innerHTML = cmd;
