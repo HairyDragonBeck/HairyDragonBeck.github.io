e7f536c4ea73cec75a21b21c896befbbfa13f7e8
HairyDragonBeck <hairybeck2@gmail.com>
2017-11-08 10:51:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var inputTextVar = document.getElementById('inputText');
 function submitCommand() {
     var inputText  = inputTextVar.value;
     var formInput = document.getElementsByName('formInput').value;
-    if (formInput == 'SpinTheBottle') {
+    if (formInput = 'SpinTheBottle') {
         window.open('https://hairydragonbeck.github.io/SpinTheBottle/spinthebottle.html', '_self');
         }
     
