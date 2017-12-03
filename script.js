b915969db4fb3c36c095a58742a681dae27f841e
Rayzr522 <rayzr522@gmail.com>
2017-12-03 18:39:29 -0500

    Cleaned up all the things

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 //var cmd = document.getElementById('inputText').value;
 var inputTextVar = document.getElementById('inputText');
 function submitCommand(valued) {
-    var inputText  = inputTextVar.value;
+    var inputText = inputTextVar.value;
     var formInput = document.getElementsByName('formInput').value;
     if (valued == 'SpinTheBottle') {
-       // window.open('https://hairydragonbeck.github.io/SpinTheBottle/spinthebottle.html', '_blank');
+        // window.open('https://hairydragonbeck.github.io/SpinTheBottle/spinthebottle.html', '_blank');
         alert(valued);
-        }
-    
+    }
+
 }
-  
-var location = {"SpinTheBottle":"/SpinTheBottle/spinthebottle.html"};
+
+// var location = { "SpinTheBottle": "/SpinTheBottle/spinthebottle.html" };
