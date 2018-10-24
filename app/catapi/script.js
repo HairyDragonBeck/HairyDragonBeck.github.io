8563c7e684a7736478f469412faadf6bfb3373a0
loltatoez <hairybeck2@gmail.com>
2018-10-24 10:22:18 -0400

    Update script.js

--- app/catapi/script.js
+++ app/catapi/script.js
@@ -1,1 +1,11 @@
-
+function getCat(){
+ // Cat API link http://aws.random.cat/meow 
+  var httpreq = new XMLHttpRequest(); // Initilize the request
+  httpreq.onreadystatechange = function() { // Where to put the code
+    if (this.readyState == 4 && this.status == 200) { // Make sure there isn't any errors
+      //document.getElementById('image').innerHTML = 
+    }
+  };
+  httpreq.open('GET', 'http://aws.random.cat/meow, true);
+  httpreq.send();
+}
