0f1032ada6d950216a0924900a1367179654e931
loltatoez <hairybeck2@gmail.com>
2018-10-24 10:09:27 -0400

    Create index.js

---
+++ app/catapi/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title> Random Cat </title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <script src="script.js" type="text/javascript">
+    </script>
+  </head>
+  <body>
+    <img alt="Randomly generated picture of a cat" id="image" class="image">
+    <button id="generateButton" class="btn generateButton">
+  </body>
+</html>
