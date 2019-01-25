5b601d67016eba18c13155d8600b84abd5c44e70
loltatoez <hairybeck2@gmail.com>
2019-01-25 09:19:23 -0500

    Update script.js

--- app/piglatin/src/script.js
+++ app/piglatin/src/script.js
@@ -3,18 +3,20 @@ var submit = document.getElementById('submit');
 
 
 submit.addEventListener('click', function() {
-	 var inputValue = input.value; // Get input value
-   var inputArray = inputValue.split(''); // Makes each character it's own in an array
-   var shifted = inputArray.shift(); // Removes first letter
-   inputArray.push(shifted); // Adds first letter to end
-   inputArray.push('a'); // Adds a to last letter
-   inputArray.push('y'); // Adds y to last letter
-   
-   		inputArray.reduce(joinTogether());
-   
-   console.log(inputArray)
-});
+  var inputValue = input.value; // Get input value
+  var inputArray = inputValue.split(''); // Makes each character it's own in an array
+  var shifted = inputArray.shift(); // Removes first letter
+  inputArray.push(shifted); // Adds first letter to end
+  inputArray.push('a'); // Adds a to last letter
+  inputArray.push('y'); // Adds y to last letter
 
-function joinTogether(value, out) {
- 	return value + out;
-}
+  console.log(inputArray);
+  
+  for (var i = 0; i < inputArray.length; i++) {
+  	var inputJoined = inputArray[i];
+    document.getElementById('e').innerHTML = inputJoined;
+    //console.log(inputJoined);
+  }
+  //inputArray.join("");
+  document.getElementById('egg').innerHTML = inputArray;
+});
