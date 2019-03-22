94c5ae4d81ac031e40e9cc8a878981421ce43b46
loltatoez <hairybeck2@gmail.com>
2019-03-22 13:09:26 -0400

    Update script.js

--- app/piglatin/src/script.js
+++ app/piglatin/src/script.js
@@ -3,20 +3,20 @@ var input = document.getElementById('input');
 var submit = document.getElementById('submit');
 
 submit.addEventListener('click', function() {
-  var inputValue = input.value; // Get input value
-  var inputArray = inputValue.split(''); // Makes each character it's own in an array
+
+    var inputValue = input.value; // Get input value
+  var inputString = translateWord(inputValue);
+  document.getElementById('egg').innerHTML = inputString;
+});
+
+function translateWord(word) {
+
+  var inputArray = word.split(''); // Makes each character it's own in an array
   var shifted = inputArray.shift(); // Removes first letter
   inputArray.push(shifted); // Adds first letter to end
   inputArray.push('a'); // Adds a to last letter
   inputArray.push('y'); // Adds y to last letter
 
 	var inputString = inputArray.join('');
-  console.log(inputArray.join(''));
-  
- /* for (var i = 0; i < inputArray.length; i++) {
-  	//var inputJoined = inputArray[i];
- //   document.getElementById('e').innerHTML = inputJoined;
-    //console.log(inputJoined);
-  } */
-  document.getElementById('egg').innerHTML = inputString;
-});
+  return inputString;
+}
